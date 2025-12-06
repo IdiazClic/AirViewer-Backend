a2103e6e0b39df3dc42dd90f2897e9fab8b043b0
Silvana Becerra <sil.inlove02@gmail.com>
2025-12-06 12:41:22 -0500

    Create necessary directories for data and model
    Added commands to create necessary directories for data and model.

--- render_build.sh
+++ render_build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# 1. Generar la data histórica necesaria
+# 🛑 NUEVO: CREAR LAS CARPETAS NECESARIAS
+mkdir -p data
+mkdir -p model
+
+# 1. Generar la data histórica necesaria (Ahora la carpeta 'data' existe)
 python generate_data.py
 
-# 2. Entrenar el modelo LSTM y guardar el .h5 y .pkl
+# 2. Entrenar el modelo LSTM y guardar el .h5 y .pkl (Ahora la carpeta 'model' existe)
 python ml_model.py
